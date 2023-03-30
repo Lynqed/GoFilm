1347823dfb28073e57b5b6d380951a520e0645ac
kesyavlad <kesyavlad74@gmail.com>
2023-03-30 10:46:01 +0300

    fix animation in project page

--- src/page/Project/SlideOne/index.tsx
+++ src/page/Project/SlideOne/index.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./style.module.scss";
 import image from "assets/image/project.png";
-const SlideOne = () => {
+import cn from "classnames";
+import { ICommonProps } from "types";
+interface IProps extends ICommonProps {}
+const SlideOne = (props: IProps) => {
+  const { end } = props;
+  const [show, setShow] = useState<null | boolean>(null);
+  useEffect(() => {
+    setShow(true);
+  }, []);
   return (
-    <div className={style.container}>
-      <div className={style.boxImage}>
-        <img src={image} alt={"testimg"} className={style.image} />
+    <div
+      className={cn(style.container, {
+        [style.show]: show === true,
+        [style.hide]: end,
+      })}
+    >
+      <div className={style.innerContainer} />
+      <div className={style.content}>
+        <div className={style.boxImage}>
+          <img src={image} alt={"testimg"} className={style.image} />
+        </div>
       </div>
     </div>
   );
--- src/page/Project/SlideTwo/index.tsx
+++ src/page/Project/SlideTwo/index.tsx
@@ -1,38 +1,53 @@
-import React from 'react';
-import style from './style.module.scss';
-import imageProject from 'assets/image/projectImg/imgProject.png';
+import React, { useEffect, useState } from "react";
+import style from "./style.module.scss";
+import imageProject from "assets/image/projectImg/imgProject.png";
+import { ICommonProps } from "types";
+import cn from "classnames";
 
 const image = [
   { image: imageProject },
   { image: imageProject },
-  { image: imageProject }
+  { image: imageProject },
 ];
+interface IProps extends ICommonProps {}
+export const imagesSliderList = "images-list";
 
-export const imagesSliderList = 'images-list';
-
-const SlideTwo = () => {
+const SlideTwo = (props: IProps) => {
+  const { end } = props;
+  const [show, setShow] = useState<null | boolean>(null);
+  useEffect(() => {
+    setShow(true);
+  }, []);
   return (
-    <div className={style.container}>
-      <div className={style.boxText}>
-        <p className={style.headerText}>
-          Check the main <span className={style.orangeText}>details</span>
-        </p>
-        <p className={style.mainText}>
-          We helped children create their place in the world by
-          <br /> giving them the power to color themselves into it.
-        </p>
-      </div>
-      <div id={imagesSliderList} className={style.boxProjects}>
-        {image.map((el, i) => (
-          <div
-            key={i}
-            style={{
-              animationDelay: `${i * 0.15}s`
-            }}
-          >
-            <img src={el.image} alt="projectImg" />
-          </div>
-        ))}
+    <div
+      className={cn(style.container, {
+        [style.show]: show === true,
+        [style.hide]: end,
+      })}
+    >
+      <div className={style.innerContainer} />
+      <div className={style.content}>
+        <div className={style.boxText}>
+          <p className={style.headerText}>
+            Check the main <span className={style.orangeText}>details</span>
+          </p>
+          <p className={style.mainText}>
+            We helped children create their place in the world by
+            <br /> giving them the power to color themselves into it.
+          </p>
+        </div>
+        <div id={imagesSliderList} className={style.boxProjects}>
+          {image.map((el, i) => (
+            <div
+              key={i}
+              style={{
+                animationDelay: `${i * 0.15}s`,
+              }}
+            >
+              <img src={el.image} alt="projectImg" />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
