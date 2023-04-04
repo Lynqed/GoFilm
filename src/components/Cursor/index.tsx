a6c7ffaeacac262c08d7d0d5de0210c360713f74
kesyavlad <kesyavlad74@gmail.com>
2023-04-04 10:30:39 +0300

    fix cursor, add validation for input in SlideSix

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useRef } from 'react';
-import style from './style.module.scss';
+import React, { useEffect, useRef } from "react";
+import style from "./style.module.scss";
 const Cursor = () => {
   const cursorRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    document.addEventListener('mousemove', (event) => {
+    document.addEventListener("mousemove", (event) => {
       const { clientX, clientY } = event;
       if (cursorRef.current != null) {
-        const mouseX = clientX - cursorRef.current.clientWidth / 2;
-        const mouseY = clientY - cursorRef.current.clientHeight / 2;
-        cursorRef.current.style.transform = `translate3d(${mouseX}px,${mouseY}px, 0)`;
+        const mouseX = clientX - cursorRef.current.clientWidth / 2 + 17;
+        const mouseY = clientY - cursorRef.current.clientHeight / 2 + 15;
+        cursorRef.current.style.transform = `translate3d(${mouseX}px,${mouseY}px, 0) `;
       }
     });
   }, []);
+
   return <div className={style.cursor} ref={cursorRef} />;
 };
 
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,16 +4,32 @@ interface IProps {
   placeholder: string;
   type: string;
   name: string;
+  value?: string;
+  onChange: (str: string) => void;
+  status?: boolean;
+  title?: string;
 }
 
-const Input: FC<IProps> = ({ placeholder, type }) => {
+const Input: FC<IProps> = ({
+  placeholder,
+  type,
+  value,
+  onChange,
+  status,
+  title,
+}) => {
   return (
-    <input
-      placeholder={placeholder}
-      type={type}
-      className={style.input}
-      autoComplete="on"
-    />
+    <div className={style.boxInput}>
+      <input
+        placeholder={placeholder}
+        type={type}
+        className={status ? style.input : style.inputError}
+        autoComplete="on"
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+      />
+      {status ? null : <p className={style.textError}>*{title}</p>}
+    </div>
   );
 };
 
